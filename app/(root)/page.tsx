043e452642d1a64af61bbdca4c53e8857125c4de
Aditya Prakash <100155975+aditya-prakash-git@users.noreply.github.com>
2024-05-17 21:26:48 +0530

    add new component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+
+    const loggedIn = {firstName : 'Aditya'};
   return (
-    <div>Home</div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || 'Guest'}
+                subtext = "Manage your account efficiently."
+
+                
+                
+                
+                />
+
+            </header>
+
+        </div>
+    
+    
+    </section>
   )
 }
 

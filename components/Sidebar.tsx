175cf9b47fd7387bec4b05a8ce097d1486464a84
Aditya Prakash <100155975+aditya-prakash-git@users.noreply.github.com>
2024-05-19 21:33:14 +0530

    implement sidenav

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,7 +10,7 @@ const Sidebar = ({user}: SiderbarProps) => {
   return (
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
-            <Link href ="/" className="mb-12 cursor-pointer items-center gap-2" >
+            <Link href ="/" className="mb-12 cursor-pointer items-center gap-2 flex" >
                 <Image
                 src="/icons/logo.svg"
                 width={34}
@@ -49,8 +49,10 @@ const Sidebar = ({user}: SiderbarProps) => {
                 )
             } )}
             
-            
+            USER
         </nav>
+
+        FOOTER
 
 
     </section>
